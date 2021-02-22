45b26c6192caa6a4c44a38b827a13e1af8d8ea60
Alan Stokes <astokes@godaddy.com>
2021-02-22 13:05:49 -0600

    Adding linkedlist stack

--- src/main/java/dsAndAlgos/linkedlist/LinkedList.java
+++ src/main/java/dsAndAlgos/linkedlist/LinkedList.java
@@ -55,11 +55,11 @@ public class LinkedList {
         public int val;
         public ListNode next;
 
-        ListNode(int val) {
+        public ListNode(int val) {
             this.val = val;
         }
 
-        ListNode(int val, ListNode next) {
+        public ListNode(int val, ListNode next) {
             this.val = val;
             this.next = next;
         }
--- src/main/java/dsAndAlgos/stack/LinkedListStack.java
+++ src/main/java/dsAndAlgos/stack/LinkedListStack.java
@@ -1,6 +1,38 @@
 package dsAndAlgos.stack;
 
+import dsAndAlgos.linkedlist.LinkedList;
+import dsAndAlgos.linkedlist.LinkedList.ListNode;
 
 public class LinkedListStack {
 
+    LinkedList ll;
+
+    public LinkedListStack() {
+        ll = new LinkedList();
+    }
+
+    public void push(int val) {
+        ll.addNode(new ListNode(val));
+    }
+
+    public int pop() {
+        ListNode node = ll.getHead();
+        ll.delete(node.val);
+        return node.val;
+    }
+
+    public static void main(String[] args) {
+        LinkedListStack stack = new LinkedListStack();
+        stack.push(0);
+        stack.push(1);
+        stack.push(2);
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        stack.push(5);
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+    }
 }
